b5889746507518e9ef5e621ac6789852e173925b
jnannas <jwnannas@gmail.com>
2014-09-19 03:24:56 -0400

    Created and added files for P1

---
+++ README.md
@@ -0,0 +1,14 @@
+#Project 1
+
+## Live Project URL
+[link](http:p1.jwnannas.com)  
+
+## Project Description
+Project 1 involved logistical set up and troublshooting.
+This project involved set up of Xampp, Git, Github, DigitalOcean, and a domain name.
+
+## Demo Information
+None this time
+
+##Additional Details
+None this time
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+		<title>John Nannas P1 - DWA15 Project Portfolio </title>
+		<meta charset="utf-8">
+
+		<link rel='stylesheet' href='style.css'>
+</head>
+<body>
+		<h1>John Nannas <br> CSCI E-15 Project Portfolio </h1>
+
+		<h3>Project 2 Title</h3>
+		<ul>
+			<li>View:   [LINK]</li>
+			<li>Github: [LINK]</li>
+		</ul>
+
+		<h3>Project 3 Title</h3>
+		<ul>
+			<li>View:   [LINK]</li>
+			<li>Github: [LINK]</li>
+		</ul>
+
+		<h3>Project 4 Title</h3>
+		<ul>
+			<li>View:   [LINK]</li>
+			<li>Github: [LINK]</li>
+		</ul>
+
+		<img src='JohnNannas.jpg' alt='John Nannas'>
+
+		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>I am a univeristy administrator at the University of Georgia. <br>
+		   I help coordinate the Integrated Life Sciences Program there.</p>
+
+		<h2>Why are you taking this course?</h2>
+		<p>I am really interested in web technologies, and I am <br>
+		   a Harvard Extension Master's student.</p>
+
+		<h2>What previous experience do you have with programming?</h2>
+		<p>I have one year of Java. Also some experience with PHP and Javascript. </p>
+
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I am working on a PC.</p>
+
+		<h2>Anything else we should know?</h2>
+		<p>Nope, I look forward to a great class!</p>
+</body>
+</html>
+
